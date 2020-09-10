110dd39d679189a5564728922ea03016d43d91de
Carsten König <Carsten.Koenig@hotmail.de>
2020-09-10 21:01:34 +0200

    Spielprojekt für C# hinzugefügt

---
+++ Projekte/CSharpPlayground/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSharpPlayground
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
